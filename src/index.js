8a00fe8125017f4a184a81f1b6271c8746852b92
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-06-18 15:58:49 +0100

    CLEAN UP: unused code

--- src/index.js
+++ src/index.js
@@ -1,41 +1,9 @@
 require('dotenv').config({ path: 'variables.env' });
-const cors = require('cors');
-const { verify } = require('./utils/auth');
 const createServer = require('./createServer');
-const { findUserById } = require('./services/user');
 
 (async () => {
   try {
     const { httpServer, server, app } = await createServer();
-
-    // setup middleware using the app
-    const corsOptions = {
-      credentials: true,
-      origin: process.env.FRONTEND_URL,
-      optionsSuccessStatus: 200
-    };
-
-    app.use(cors(corsOptions));
-
-    // TODO: Use express middleware to populate current user (JWT)
-    app.use(async (req, res, next) => {
-      const { token } = req.headers;
-
-      if (token) {
-        const { id } = await verify(token);
-        req.userId = id;
-      }
-      next();
-    });
-
-    // 2. create a middleware that populates the user in the request
-    app.use(async (req, res, next) => {
-      // if they aren't logged in, skip this
-      if (!req.userId) return next();
-      const user = await findUserById(req.id);
-      req.user = user;
-      next();
-    });
 
     httpServer.listen(
       {
