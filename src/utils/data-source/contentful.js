26278bd6e24794c1a86f3d2a591660c5fa31467b
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-11-28 17:32:22 +0100

    🐛 FIX: modify current blog fetching to include feature

--- src/utils/data-source/contentful.js
+++ src/utils/data-source/contentful.js
@@ -23,7 +23,7 @@ class ContentfulAPI extends RESTDataSource {
 
   async entry(id) {
     const data = await this.get(
-      `/entries/${id}?select=sys.id,fields.title,fields.body,fields.createdAt,fields.tags,fields.author,fields.authorRole`
+      `/entries/${id}?select=sys.id,fields.title,fields.body,fields.createdAt,fields.tags,fields.author,fields.authorRole,fields.feature`
     );
     const entries = JSON.parse(data);
     return entries;
@@ -31,7 +31,7 @@ class ContentfulAPI extends RESTDataSource {
 
   async entriesByContentType(content_type = 'fitness', limit = 10, skip = 0) {
     const data = await this.get(
-      `/entries?content_type=${content_type}&limit=${limit}&skip=${skip}&select=sys.id,fields.title,fields.body,fields.createdAt,fields.tags,fields.author,fields.authorRole,fields.image`
+      `/entries?content_type=${content_type}&limit=${limit}&skip=${skip}&select=sys.id,fields.title,fields.body,fields.createdAt,fields.tags,fields.author,fields.authorRole,fields.image,fields.feature`
     );
     const entries = JSON.parse(data);
     return entries;
